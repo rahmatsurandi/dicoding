081e9b91c0035a887cd331ad5814bdb296eeb4cc
randi <rahmatsurandi1986@gmail.com>
2022-07-29 03:57:25 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+document.getElementById("h1c").onclick = function () {
+  document.getElementById("h1c").innerHTML = "Hyper text markup language".toUpperCase();
+  document.getElementById("h1c").style.backgroundColor = "grey";
+  document.getElementById("h1c").style.transform = "scale(1.2)";
+  document.getElementById("h1c").style.fontWeight = "100";
+  document.getElementById("h1c").style.color = "black";
+  document.getElementById("h1c").style.transition = "0.5s";
+};
+document.getElementsByClassName("h12")[0].onclick = function () {
+  document.getElementsByClassName("h12")[0].innerHTML = "case cading style sheet".toUpperCase();
+  document.getElementsByClassName("h12")[0].style.backgroundColor = "grey";
+  document.getElementsByClassName("h12")[0].style.color = "black";
+  document.getElementsByClassName("h12")[0].style.transform = "scale(1.2)";
+  document.getElementsByClassName("h12")[0].style.transition = "0.5s";
+  document.getElementsByClassName("h12")[0].style.fontWeight = "100";
+};
+document.getElementsByClassName("h13")[0].onclick = function () {
+  document.getElementsByClassName("h13")[0].innerHTML = "javascript".toUpperCase();
+  document.getElementsByClassName("h13")[0].style.color = "black";
+  document.getElementsByClassName("h13")[0].style.backgroundColor = "grey";
+  document.getElementsByClassName("h13")[0].style.transform = "scale(1.2)";
+  document.getElementsByClassName("h13")[0].style.transition = "0.5s";
+  document.getElementsByClassName("h13")[0].style.fontWeight = "100";
+};
